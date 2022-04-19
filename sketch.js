f9a9ff1ca6ba721835e9afe3270cc619516b49e7
Eduardo de Carvalho <63818354+Aluno-Carvalho@users.noreply.github.com>
2022-04-19 19:11:24 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles, blast;
+var cars = [];
+var contador = 0;
+
+function preload() {
+  backgroundImage = loadImage("./assets/planodefundo.png");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/PISTA.jpg");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle1Image = loadImage("./assets/obstacle1.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png");
+  lifeImage = loadImage("./assets/life.png");
+  blast = loadImage("./assets/blast.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  contador += 1;
+  console.log(contador);
+
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+
+  if (gameState === 2) {
+    game.showLeaderboard();
+    game.end();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
